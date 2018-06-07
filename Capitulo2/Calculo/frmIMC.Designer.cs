1218e6e9d997dab1e2fed25b66f70d7f1d697a59
Bruno Henrique <bhrodrigues@svlabs.com.br>
2018-06-07 16:58:22 -0300

    Commit 07/06/2018 - 2
    Início da segunda atividade do módulo 2

---
+++ Capitulo2/Calculo/frmIMC.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Calculo
+{
+    partial class frmIMC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpboxGender = new System.Windows.Forms.GroupBox();
+            this.rdbFemale = new System.Windows.Forms.RadioButton();
+            this.rdoMale = new System.Windows.Forms.RadioButton();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.lblIMC = new System.Windows.Forms.Label();
+            this.grpboxGender.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpboxGender
+            // 
+            this.grpboxGender.Controls.Add(this.rdoMale);
+            this.grpboxGender.Controls.Add(this.rdbFemale);
+            this.grpboxGender.Location = new System.Drawing.Point(12, 12);
+            this.grpboxGender.Name = "grpboxGender";
+            this.grpboxGender.Size = new System.Drawing.Size(170, 42);
+            this.grpboxGender.TabIndex = 0;
+            this.grpboxGender.TabStop = false;
+            this.grpboxGender.Text = "Sexo";
+            // 
+            // rdbFemale
+            // 
+            this.rdbFemale.AutoSize = true;
+            this.rdbFemale.Location = new System.Drawing.Point(6, 14);
+            this.rdbFemale.Name = "rdbFemale";
+            this.rdbFemale.Size = new System.Drawing.Size(67, 17);
+            this.rdbFemale.TabIndex = 0;
+            this.rdbFemale.TabStop = true;
+            this.rdbFemale.Text = "&Feminino";
+            this.rdbFemale.UseVisualStyleBackColor = true;
+            this.rdbFemale.CheckedChanged += new System.EventHandler(this.rdbFemale_CheckedChanged);
+            // 
+            // rdoMale
+            // 
+            this.rdoMale.AutoSize = true;
+            this.rdoMale.Location = new System.Drawing.Point(79, 14);
+            this.rdoMale.Name = "rdoMale";
+            this.rdoMale.Size = new System.Drawing.Size(73, 17);
+            this.rdoMale.TabIndex = 1;
+            this.rdoMale.TabStop = true;
+            this.rdoMale.Text = "&Masculino";
+            this.rdoMale.UseVisualStyleBackColor = true;
+            this.rdoMale.CheckedChanged += new System.EventHandler(this.rdbFemale_CheckedChanged);
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(188, 12);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(37, 13);
+            this.lblHeight.TabIndex = 1;
+            this.lblHeight.Text = "&Altura:";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(188, 34);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(80, 20);
+            this.txtHeight.TabIndex = 2;
+            this.txtHeight.Leave += new System.EventHandler(this.txtHeight_Leave);
+            // 
+            // lblIMC
+            // 
+            this.lblIMC.AutoSize = true;
+            this.lblIMC.Location = new System.Drawing.Point(112, 96);
+            this.lblIMC.Name = "lblIMC";
+            this.lblIMC.Size = new System.Drawing.Size(35, 13);
+            this.lblIMC.TabIndex = 3;
+            this.lblIMC.Text = "label1";
+            this.lblIMC.Visible = false;
+            // 
+            // frmIMC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 157);
+            this.Controls.Add(this.lblIMC);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.grpboxGender);
+            this.Name = "frmIMC";
+            this.Text = "Cálculo peso ideal";
+            this.grpboxGender.ResumeLayout(false);
+            this.grpboxGender.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpboxGender;
+        private System.Windows.Forms.RadioButton rdoMale;
+        private System.Windows.Forms.RadioButton rdbFemale;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label lblIMC;
+    }
+}
+
---
+++ Capitulo2/Calculo/frmIMC.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculo
+{
+    public partial class frmIMC : Form
+    {
+        public frmIMC()
+        {
+            InitializeComponent();
+        }
+
+        
+        public void calculateIMC()
+        {
+            double heigth = 0, imc;
+            try
+            {
+                String teste = txtHeight.Text.Replace(".","").Replace(",","");
+                teste = teste.Substring(0, teste.Length - 2) + "," + teste.Substring(teste.Length - 2);
+                heigth = Convert.ToDouble(teste);
+            }
+            catch(Exception ex)
+            {
+                new CancelEventArgs().Cancel = true;
+            }
+
+            if (rdbFemale.Checked)
+            {
+                imc = (62.1 * heigth) - 44.7;
+            }
+            else
+            {
+                imc = (72.7 * heigth) - 58;
+            }
+            lblIMC.Text = String.Format("{0:n2}", imc);
+            lblIMC.Visible = true;
+        }
+
+        private void rdbFemale_CheckedChanged(object sender, EventArgs e)
+        {
+            if (txtHeight.Text.Length !=0)
+            {
+                calculateIMC();
+            }
+        }
+
+        private void txtHeight_Leave(object sender, EventArgs e)
+        {
+            calculateIMC();
+        }
+    }
+}
