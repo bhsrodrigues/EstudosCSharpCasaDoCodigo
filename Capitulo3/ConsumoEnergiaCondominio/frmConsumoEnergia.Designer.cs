c20a6fdd199dfb1b23453dbabe5a6c23a3a6f427
bhsrodrigues <bruno-henrique-1992@hotmail.com>
2018-07-16 16:27:06 -0300

    Commit 16/07/2018
    Desenvolvimento de Windows
    Adendo: utilizado o conceito de branches no VSTS. Posteriormente usar apenas a linha de comando.

--- Capitulo3/ConsumoEnergiaCondominio/frmConsumoEnergia.Designer.cs
+++ Capitulo3/ConsumoEnergiaCondominio/frmConsumoEnergia.Designer.cs
@@ -32,6 +32,14 @@
             this.txtHouseNumber = new System.Windows.Forms.TextBox();
             this.lblConsumo = new System.Windows.Forms.Label();
             this.txtConsumption = new System.Windows.Forms.TextBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.columnHouse = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnConsumption = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnDiscount = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblHouseNumber
@@ -66,17 +74,85 @@
             this.txtConsumption.Size = new System.Drawing.Size(72, 20);
             this.txtConsumption.TabIndex = 3;
             // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(263, 12);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(75, 23);
+            this.btnRegister.TabIndex = 4;
+            this.btnRegister.Text = "Registrar";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.columnHouse,
+            this.columnConsumption,
+            this.columnDiscount});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 42);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(325, 185);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // columnHouse
+            // 
+            this.columnHouse.HeaderText = "Casa";
+            this.columnHouse.Name = "columnHouse";
+            this.columnHouse.ReadOnly = true;
+            this.columnHouse.Width = 94;
+            // 
+            // columnConsumption
+            // 
+            this.columnConsumption.HeaderText = "Consumo";
+            this.columnConsumption.Name = "columnConsumption";
+            this.columnConsumption.ReadOnly = true;
+            this.columnConsumption.Width = 94;
+            // 
+            // columnDiscount
+            // 
+            this.columnDiscount.HeaderText = "Desconto";
+            this.columnDiscount.Name = "columnDiscount";
+            this.columnDiscount.ReadOnly = true;
+            this.columnDiscount.Width = 94;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(13, 242);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(118, 23);
+            this.btnProcess.TabIndex = 6;
+            this.btnProcess.Text = "Processar Dados";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(158, 247);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(164, 13);
+            this.lblTotal.TabIndex = 7;
+            this.lblTotal.Text = "Total de consumo sem desconto:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(359, 287);
+            this.ClientSize = new System.Drawing.Size(347, 276);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnRegister);
             this.Controls.Add(this.txtConsumption);
             this.Controls.Add(this.lblConsumo);
             this.Controls.Add(this.txtHouseNumber);
             this.Controls.Add(this.lblHouseNumber);
             this.Name = "Form1";
             this.Text = "Consumo de Energia de um Condomínio";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -88,6 +164,13 @@
         private System.Windows.Forms.TextBox txtHouseNumber;
         private System.Windows.Forms.Label lblConsumo;
         private System.Windows.Forms.TextBox txtConsumption;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnHouse;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnConsumption;
+        private System.Windows.Forms.DataGridViewButtonColumn columnDiscount;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Label lblTotal;
     }
 }
 
